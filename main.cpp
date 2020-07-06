3e358e1e04ccd0d9959eb89455baffd8fe02db60
simonryu328 <45925379+simonryu328@users.noreply.github.com>
2020-07-06 05:50:39 -0700

    Added Serial, removed fine tune count

--- main.cpp
+++ main.cpp
@@ -5,8 +5,7 @@
 #include "util_timer.h"
 #include "util_motor.h"
 #include "pin_define.h"
-
-#define PWM_FREQUENCY 1000
+#include "util_serial.h"
 
 Servo servo;
 
@@ -15,51 +14,13 @@ void setup() {
   lcd_init();
   timer_init();
 
-  #if 0
-  pinMode(SERVOPIN, OUTPUT);
-  servo.attach(SERVOPIN);
-  #endif
-
   // sonic for tape sensor
   pinMode(TRIGGERPIN, OUTPUT);
   pinMode(ECHOPIN, INPUT_PULLDOWN);
   pinMode(TAPESENSOR, INPUT_PULLDOWN);
-
 }
 
 void loop() {
-
-// testing servo
-#if 0
-  servo.write(0);
-  lcd_print_number(-90);
-  delay(2000);
-  
-  servo.write(45);
-  lcd_print_number(-45);
-  delay(2000);
-
-  servo.write(90);
-  lcd_print_number(0);
-  delay(2000);
-#endif
-
-#if 0
-  uint32_t distance_cm = get_distance();
-  lcd_print_str_number("distance cm:", distance_cm);
-  delay(10);
-#endif
-
-#if 0
-  uint32_t pin_read = analogRead(V_3_3);
-  lcd_print_str_number("Current voltage", (double)pin_read);
-  delay(3000);
-#endif
-
-#if 0
-  lcd_print_str_number("Tape Sensor: ", tape_reading);
-  delay(300);
-#endif
 
 #if 1
   if (state != STATE_NULL) {
@@ -79,19 +40,23 @@ void loop() {
     motor_stop();
     lcd_print_str_number("STATE INIT", distance_reading);
     if (read_count > 100) {
-      //state = STATE_TUNE_START;
-      state = STATE_SERVO_TEST;
+      state_debug_record();
+      state = STATE_FORWARD_RUN;
+      //state = STATE_SERVO_TEST;
     }
 
   } else if (state == STATE_FORWARD_RUN) {
-    lcd_print_str_number("STATE RUN", distance_reading);
+    lcd_print_str_num4("STATE RUN", distance_reading, distance_target, 0, distance_reading_instant);
     motor_run(RUN_FORWARD_SPEED);
 
     if(distance_reading > distance_target + DISTANCE_MARGIN) {
+      state_debug_record();
       state = STATE_TUNE_START;
       //state = STATE_NULL;
     } else {
-      distance_target = distance_reading;
+      if(distance_reading < distance_target) {
+        distance_target = distance_reading;
+      }
       state = STATE_FORWARD_RUN;
     }
 
@@ -99,19 +64,22 @@ void loop() {
     
     motor_stop();
     rotate_count = 0;
-    rotate_count_done = ROTATE_COUNT_90DEG;
+    rotate_count_done = ROTATE_COUNT_180DEG;
     rotate_speed = ROTATE_SPEED_MIN;
+    state_debug_record();
     state = STATE_ROTATE_START_RIGHT;
     lcd_print_str_number("STATE TAPE DET", distance_reading);
 
   } else if (state == STATE_ROTATE_START_RIGHT) {
 
     motor_rotate_right(rotate_speed);
+    state_debug_record();
     state = STATE_ROTATING;
 
   } else if (state == STATE_ROTATE_START_LEFT) {
 
     motor_rotate_left(rotate_speed);
+    state_debug_record();
     state = STATE_ROTATING;
 
   } else if (state == STATE_ROTATING) {
@@ -119,55 +87,68 @@ void loop() {
     //lcd_print_str_number("STATE ROTATING", distance_reading);
     if (rotate_count > rotate_count_done) {
       if (tune_flag) {
+        state_debug_record();
         state = STATE_ROTATE_TUNE_DONE;
       } else {
+        state_debug_record();
         state = STATE_ROTATE_DONE;
       }
     }
 
   } else if (state == STATE_ROTATE_TUNE_DONE) {
-    
-    rotate_count = 0;
+
     motor_stop();
-    if (distance_reading < distance_target + DISTANCE_MARGIN) {
-      tune_flag = 0;
-      state = STATE_FORWARD_RUN;
-    } else {
+    analogWriteFrequency(100000);
+    delay(10);
+    sonic_measure_count_tune_done++;
+
+    if(sonic_measure_count_tune_done > NUM_READING) {
+      lcd_print_str_num4("STATE TUNE STEP DONE", distance_reading, distance_target, tune_flag, distance_reading_instant);
       rotate_count = 0;
-      rotate_count_done = ROTATE_COUNT_10DEG;
-      rotate_speed = ROTATE_SPEED_MIN;
-      state = STATE_ROTATE_START_LEFT;
-      tune_flag++;
-      lcd_print_str_number("STATE TUNE STEP DONE", tune_flag);
-      //state = STATE_NULL;
+      if (distance_reading < distance_target + DISTANCE_MARGIN) {
+
+        tune_flag = 0;
+        distance_target = distance_reading;
+        state_debug_record();
+        state = STATE_FORWARD_RUN;
+
+      } else {
+
+        rotate_count = 0;
+        rotate_count_done = ROTATE_COUNT_TUNE_LEFT;
+        rotate_speed = ROTATE_SPEED_MIN;
+        state_debug_record();
+        state = STATE_ROTATE_START_LEFT;
+        tune_flag++;
+        //state = STATE_NULL;
+      }
+      sonic_measure_count_tune_done = 0;
     }
 
   } else if (state == STATE_ROTATE_DONE) {
     
     motor_stop();
-    state = STATE_FORWARD_RUN;
+    distance_target = DISTANCE_MAX;
+    state_debug_record();
+    state = STATE_TUNE_START;
 
   } else if (state == STATE_TUNE_START) {
     
     motor_stop();
-    lcd_print_str_number("STATE TUNE", distance_reading);
+    lcd_print_str_num4("STATE TUNE START", distance_reading, distance_target, 0, 0);
     rotate_count = 0;
-    rotate_count_done = ROTATE_COUNT_60DEG;
+    sonic_measure_count_tune_done = 0;
+    rotate_count_done = ROTATE_COUNT_TUNE_RIGHT;
     rotate_speed = ROTATE_SPEED_MIN;
+    state_debug_record();
     state = STATE_ROTATE_START_RIGHT;
     tune_flag = 1;
-
-  } else if (state == STATE_FOUND) {
-
-    lcd_print_str_number("STATE FOUND", distance_reading);
-    distance_target = distance_reading;
-    state = STATE_FORWARD_RUN;
 
   } else if (state == STATE_STOP) {
 
     lcd_print_str_number("STATE STOP", distance_reading);
     motor_stop();
-    distance_reading = DISTANCE_MAX;
+    distance_target = DISTANCE_MAX;
 
   } else if (state == STATE_SERVO_TEST) {
     // servo test
@@ -202,6 +183,7 @@ void loop() {
     delay(2000);
 
   } else if (state == STATE_NULL) {
+
     motor_stop();
     lcd_print_str_number("STATE NULL", distance_reading);
     delay(1000);
@@ -211,6 +193,9 @@ void loop() {
     delay(1000);
     lcd_print_str_number("count", read_count);
     delay(1000);
+
+    state_debug_print();
+
   }
   #endif
 }
